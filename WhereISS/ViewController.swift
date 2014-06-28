bc60857b213775cbaa47d33ae9214c250a1b71b6
jmyrose <jmyrose@tangogroup.com>
2014-06-28 10:12:31 -0600

    Initial commit

---
+++ WhereISS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereISS
+//
+//  Created by Jackie Myrose on 6/28/14.
+//  Copyright (c) 2014 jmyrose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
